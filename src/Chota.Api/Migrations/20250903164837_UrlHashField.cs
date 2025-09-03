79996286e8c1caa51f82ebf79ae188858f8d6c8d
Asad Ateeque <319157+aateeque@users.noreply.github.com>
2025-09-03 18:32:30 +0100

    Add db migration for url hash

---
+++ src/Chota.Api/Migrations/20250903164837_UrlHashField.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Chota.Api.Migrations
+{
+    /// <inheritdoc />
+    public partial class UrlHashField : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "LongUrlHash",
+                table: "ShortUrls",
+                type: "text",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "LongUrlHash",
+                table: "ShortUrls");
+        }
+    }
+}
